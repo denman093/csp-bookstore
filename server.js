210fff7c568783f20149a221c8b8b1067a528bc7
Dennis Pillar <pillard@csp.edu>
2020-05-03 20:49:45 -0500

    update

--- server.js
+++ server.js
@@ -6,14 +6,12 @@ const path = require('path');
 const app = express();
 
 const PORT = process.env.PORT || 5000;
-
 const routes = require('./routes/api');
 
-const MONGODB_URI = 'mongodb+srv://denman093:denman093@cluster0-7rogf.mongodb.net/test?retryWrites=true&w=majority';
-//process.env.
-mongoose.connect(MONGODB_URI || 'mongodb://localhost/csp-bookstore-users', {
+//MONGODB_URI = 'mongodb+srv://denman093:denman093@cluster0-7rogf.mongodb.net/test?retryWrites=true&w=majority';
+
+mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
     useCreateIndex: true
 });
 
@@ -30,7 +28,6 @@ app.use(morgan('tiny'));
 app.use('/api', routes);
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
     app.use('*', express.static('client/build'));
 
     app.get('*', (req,res) => {
